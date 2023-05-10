f1a6a0b00b18f290f7778754db89c6824a0f08d9
Jamard Theo <theo.jamard@etu.u-bordeaux.fr>
2023-05-10 09:31:09 +0200

    dirty board display

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {View, Text} from "react-native";
+import { View, Image } from "react-native";
 import Title from "./components/Title";
+import Wall from "./assets/sprites/box.png";
+import Empty from "./assets/sprites/empty.png";
+import Character from "./assets/sprites/characterWalking_1.png";
 
 export default function App() {
-    return (
 
-        <View>
-            <Title name={"SOKOBAN APP"} subName={"par Théo Jamard & Paul Gubbiotti"}></Title>
-        </View>
+    const board = [
+        ['W','W','W','W','W','W','W','W'],
+        ['W','E','E','E','E','E','E','W'],
+        ['W','E','E','E','E','E','E','W'],
+        ['W','E','E','C','E','E','E','W'],
+        ['W','E','E','E','E','E','E','W'],
+        ['W','W','W','W','W','W','W','W'],
+    ]
+
+    const strToImage = (str) => {
+        switch (str) {
+            case 'W':
+                return <Image source={Wall} />;
+            case 'E':
+                return <Image source={Empty} />;
+            case 'C':
+                return <Image source={Character} />;
+            default:
+                return null;
+        }
+    }
+
+    return (
+      <View style={{ backgroundColor: '#c4d4d4' }}>
+          <Title name={"SOKOBAN APP"} subName={"par Théo Jamard & Paul Gubbiotti"}></Title>
+          {board.map((row, index) => (
+            <View key={index} style={{ flexDirection: 'row'}}>
+                {row.map((cell, index) => (
+                  <View key={index} style={{ width: 40, height: 54 }}>
+                      {strToImage(cell)}
+                  </View>
+                ))}
+            </View>
+          ))}
+      </View>
     );
 }
